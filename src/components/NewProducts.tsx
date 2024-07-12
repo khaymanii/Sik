21bdd2b37cb8a66a41f45575f038031bc6e0ab90
khaymanii <kennethakpo61@gmail.com>
2024-07-12 14:37:17 +0100

    added image

--- src/components/NewProducts.tsx
+++ src/components/NewProducts.tsx
@@ -11,59 +11,58 @@ interface productsData {
 
 const productsData: productsData[] = [
   {
-    img: "/image5.png",
-    title: "Cooperate Heel",
-    desc: "Flower Design",
+    img: "/image1.png",
+    title: "Nike Black",
+    desc: "Black Nike Sneakers",
     rating: 5,
     price: "80.00",
   },
   {
-    img: "/image5.png",
-    title: "Cooperate Heel",
-    desc: "Flower Design",
+    img: "/image1.png",
+    title: "Nike Black",
+    desc: "Black Nike Sneakers",
     rating: 5,
     price: "80.00",
   },
   {
-    img: "/image5.png",
-    title: "Cooperate Heel",
-    desc: "Flower Design",
-    rating: 5,
-    price: "80.00",
-  },
-
-  {
-    img: "/image5.png",
-    title: "Cooperate Heel",
-    desc: "Flower Design",
+    img: "/image1.png",
+    title: "Nike Black",
+    desc: "Black Nike Sneakers",
     rating: 5,
     price: "80.00",
   },
   {
-    img: "/image5.png",
-    title: "Cooperate Heel",
-    desc: "Flower Design",
+    img: "/image1.png",
+    title: "Nike Black",
+    desc: "Black Nike Sneakers",
     rating: 5,
     price: "80.00",
   },
   {
-    img: "/image5.png",
-    title: "Cooperate Heel",
-    desc: "Flower Design",
+    img: "/image1.png",
+    title: "Nike Black",
+    desc: "Black Nike Sneakers",
     rating: 5,
     price: "80.00",
   },
   {
-    img: "/image5.png",
-    title: "Cooperate Heel",
-    desc: "Flower Design",
-    rating: 4,
+    img: "/image1.png",
+    title: "Nike Black",
+    desc: "Black Nike Sneakers",
+    rating: 5,
     price: "80.00",
   },
   {
-    img: "/image5.png",
-    title: "Cooperate Heel",
-    desc: "Flower Design",
+    img: "/image1.png",
+    title: "Nike Black",
+    desc: "Black Nike Sneakers",
+    rating: 5,
+    price: "80.00",
+  },
+  {
+    img: "/image1.png",
+    title: "Nike Black",
+    desc: "Black Nike Sneakers",
     rating: 5,
     price: "80.00",
   },
@@ -73,7 +72,7 @@ export default function NewProducts() {
   return (
     <div>
       <div className="container pt-16">
-        <h2 className="font-medium text-2xl pb-4">New Products</h2>
+        <h2 className="font-bold text-2xl pb-4">New Products</h2>
         <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-col-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-10">
           {productsData.map((item, index) => {
             return (
