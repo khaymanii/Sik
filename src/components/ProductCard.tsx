1b005990b0d5066047b5a81fd7e114a7271d8088
khaymanii <kennethakpo61@gmail.com>
2024-07-12 13:58:47 +0100

    added image in png format

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import Image from "next/image";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
 interface ProductsProps {
   img: string;
@@ -15,5 +16,74 @@ export default function ProductCard({
   rating,
   price,
 }: ProductsProps) {
-  return <div>ProductCard</div>;
+  const generateRating = (rating: any) => {
+    switch (rating) {
+      case 1:
+        return (
+          <div>
+            <AiFillStar />
+            <AiOutlineStar />
+            <AiOutlineStar />
+            <AiOutlineStar />
+            <AiOutlineStar />
+          </div>
+        );
+      case 2:
+        return (
+          <div>
+            <AiFillStar />
+            <AiFillStar />
+            <AiOutlineStar />
+            <AiOutlineStar />
+            <AiOutlineStar />
+          </div>
+        );
+      case 3:
+        return (
+          <div>
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiOutlineStar />
+            <AiOutlineStar />
+          </div>
+        );
+      case 4:
+        return (
+          <div>
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiOutlineStar />
+          </div>
+        );
+      case 5:
+        return (
+          <div>
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+          </div>
+        );
+    }
+  };
+  return (
+    <div>
+      <div>
+        <Image src={img} width={200} height={300} alt={title} />
+      </div>
+      <div>
+        <h2>{title}</h2>
+        <p>{desc}</p>
+        <div>{generateRating(rating)}</div>
+        <div>
+          ${price}
+          <del>${parseInt(price) + 50}.00</del>
+        </div>
+      </div>
+    </div>
+  );
 }
